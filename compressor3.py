b6995585a56aa0b27b59030138511aab29c2c2bc
Darley Barreto <darleybarreto@gmail.com>
2017-10-30 15:58:54 -0300

    compressor3.py with bugs

--- compressor3.py
+++ compressor3.py
@@ -1,6 +1,8 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+
+from scipy.stats import multivariate_normal
 # mpl.use('Agg')
 
 from sklearn import mixture
@@ -39,7 +41,7 @@ def load_sigma_dots(data_dict,n_gauss):
 
 	return sigma_dots
 
-def train(data_dict,n_gauss, show=False):
+def train(data_dict, n_gauss, err=0.01, show=False):
 	if data_dict["data"] is None:
 		print('No data!')
 		return
@@ -52,6 +54,8 @@ def train(data_dict,n_gauss, show=False):
 	data_dict["sigma_dots"] = load_sigma_dots(data_dict,n_gauss)
 	data_dict["anti_j"] = [np.arange(data_dict["m"]) != j for j in range(data_dict["m"])]
 	data_dict["n_gauss"] = n_gauss
+	data_dict["err"] = err
+	data_dict["mvns"] = {}
 
 def get_mub_helper(data_dict):
 	# data_dict["mu"] - 
@@ -63,15 +67,40 @@ def get_mub_helper(data_dict):
 		for j in range(data_dict["m"])]
 		for k in range(data_dict["n_gauss"])])
 
+def get_mub(data_dict, i):
+	return np.array([
+		[data_dict["mub_helper"][k, j] + \
+	 	np.dot(
+		 	data_dict["sigma_dots"][k, j], data_dict["data"][i][data_dict["anti_j"][j]]
+	 	)
+		for j in range(data_dict["m"])]
+		for k in range(data_dict["n_gauss"])])
+
+def get_t(data_dict, i, rangeJ, tup_rangeJ):
+	pdfs = [data_dict["mvns"][tup_rangeJ][k].pdf(data_dict["data"][i][rangeJ]) for k in range(data_dict["n_gauss"])]
+
+	den = sum([data_dict["pi"][pos] * pdfs[pos] for pos in range(data_dict["n_gauss"])])
+	return [data_dict["pi"][k] * pdfs[k] / den for k in range(data_dict["n_gauss"])]
+
+def get_err_vec(data, completeRangeJ):
+	return np.abs((completeRangeJ - data) / data)
+
+def find_min(data, completeRangeJ):
+	errs = get_err_vec(data, completeRangeJ)
+	min_err = min(errs)
+	try:
+		return min_err, np.where(errs == min_err)[0][0]
+	except:
+		return np.inf, -1
 
 # @numba.jit(int32(int32, int32))
-@numba.jit(nopython=True, parallel=True)
+# @numba.jit(parallel=True)
 def parallel_run(i, data_dict, prefix):
 	if i % (data_dict["n"] // 100) == 0 or i == data_dict["n"] - 1:
 		print('{} {} / {}'.format(prefix, i + 1, data_dict["n"]),end='\r')
 
 	nRem = 0
-	mub = get_mub(data_dict["data"])
+	mub = get_mub(data_dict, i)
 	totalRems = 0
 	minDifs = np.zeros(data_dict["m"])
 	minDifIdxs = -np.ones(data_dict["m"])
@@ -80,16 +109,66 @@ def parallel_run(i, data_dict, prefix):
 		rangeJ = np.array([j for j in range(data_dict["m"]) if j not in minDifIdxs])
 		tup_rangeJ = tuple(rangeJ)
 
-@numba.jit(parallel=True)
+		# ---------------------------------------------------------
+		if tup_rangeJ not in data_dict["mvns"]:
+			r_mu = [data_dict["mu"][k, rangeJ] for k in range(data_dict["n_gauss"])]
+			r_sigma = [np.take(data_dict["sigma"][k, rangeJ], rangeJ, axis=1) for k in range(data_dict["n_gauss"])]
+
+			data_dict["mvns"][tup_rangeJ] = [multivariate_normal(mean, cov) for mean, cov in zip(r_mu, r_sigma)]
+		# ---------------------------------------------------------
+
+		# ---------------------------------------------------------
+		# Second bottleneck
+		# self.watches['pred_watch'].start()
+		t = get_t(data_dict, i, rangeJ, tup_rangeJ)
+		# self.watches['pred_watch'].stop()
+		# ---------------------------------------------------------
+		xb = [sum([t[k] * mub[k, j] for k in range(data_dict["n_gauss"])]) for j in range(data_dict["m"])]
+		# try:
+		#	 self.xb[i] = copy.copy(xb2)
+		# except Exception:
+		#	 print("Error")
+
+		# self.xb[i] = [sum([t[k]*mub[k, j] for k in range(self.n_gauss)]) for j in range(self.m)]
+
+		erros = get_err_vec(data_dict["data"][i], xb)
+
+		for j in range(data_dict["m"]):
+			if j not in rangeJ:
+				xb[j] = np.inf
+
+		minDif, minDifIndex = find_min(data_dict["data"][i], xb)
+		# print(type(minDif), type(minDifIndex))
+
+		if minDif > data_dict["err"]:
+			break
+
+		isErr = False
+		for j2 in minDifIdxs[minDifIdxs != -1]:
+			if erros[j2] > data_dict["err"]:
+				isErr = True
+
+		if nRem + 1 >= data_dict["m"] or isErr:
+			break
+
+		minDifs[nRem] = minDif
+		# self.minDifIdxs[i, nRem] = minDifIndex
+
+		nRem += 1
+
+		totalRems += 1
+	return minDifIdxs, minDifs, totalRems
+
+# @numba.jit(parallel=True)
 def run(data_dict,n_jobs=6):
 	data_dict["mub_helper"] = get_mub_helper(data_dict)
 	prefix = ""
 	for i in numba.prange(n_jobs):
-		for idx in range(data_dict["n"])
-			parallel_run(idx,data_dict,prefix)
+		for idx in range(data_dict["n"]):
+			parallel_run(int(data_dict["n"]/n_jobs * i + idx),data_dict,prefix)
 
 
-data_dict = load_data()
-train(data_dict,10)
+data_dict = load_data("tpc-h-5k")
+train(data_dict, 10)
 run(data_dict)
 
